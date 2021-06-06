a6f5f08d7d613c2acc481d968fd2b40d06425be0
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-06-06 12:07:16 +0100

    feat :  history api to change the url

--- web/src/main/App.js
+++ web/src/main/App.js
@@ -1,4 +1,4 @@
-import { Router, Route, Switch } from "react-router";
+import { Route, Switch, useHistory } from "react-router-dom";
 import Steps from "./pages/steps/steps";
 import Tracker from "./pages/tracker/tracker";
 import "antd/dist/antd.css";
@@ -8,88 +8,80 @@ import {
     HomeOutlined,
     SettingOutlined,
 } from "@ant-design/icons";
+
 import { Layout, Menu } from "antd";
 import { useState } from "react";
-import { createBrowserHistory } from "history";
-const browserHistory = createBrowserHistory();
-
 const { Sider } = Layout;
+
 function App() {
     const [active_keys, setItemToActive] = useState(["1"]);
     const [collapsed, onCollapse] = useState(false);
-
+    const history = useHistory()
     return (
-        <>
-            <Router history={browserHistory}>
-                <Layout>
-                    <Sider
-                        collapsible
-                        collapsed={collapsed}
-                        onCollapse={onCollapse}
+        <Layout>
+            <Sider
+                collapsible
+                collapsed={collapsed}
+                onCollapse={onCollapse}
+            >
+                <Menu
+                    theme="dark"
+                    defaultSelectedKeys={active_keys}
+                    mode="inline"
+                >
+                    <Menu.Item
+                        onClick={(item) => {
+                            item.domEvent.preventDefault();    
+                            history.push(`/home`)
+                            setItemToActive([item]);
+                        }}
+                        key="1"
+                        icon={<HomeOutlined />}
                     >
-                        <div className="logo" />
-                        <Menu
-                            theme="dark"
-                            defaultSelectedKeys={active_keys}
-                            mode="inline"
-                        >
-                            <Menu.Item
-                                onClick={(item) => {
-                                    item.domEvent.preventDefault();
-                                    setItemToActive([item]);
-                                    window.location.replace(`${window.location.origin}/home`)
-                                }}
-                                key="1"
-                                icon={<HomeOutlined />}
-                            >
-                                Home
-                            </Menu.Item>
-                            <Menu.Item
-                                onClick={(item) => {
-                                    item.domEvent.preventDefault();
-                                    setItemToActive([item]);
-                                    window.location.assign(`${window.location.origin}/steps`)
-                                }}
-                                key="2"
-                                icon={<PieChartOutlined />}
-                            >
-                                Steps
-                            </Menu.Item>
-                            <Menu.Item
-                                onClick={(item) => {
-                                    item.domEvent.preventDefault();
-                                    setItemToActive([item])
-                                    window.location.assign(`${window.location.origin}/tracker`)
-                                }}
-                                key="3"
-                                icon={<LineChartOutlined />}
-                            >
-                                Tracker
-                            </Menu.Item>
-
-                            <Menu.Item
-                                onClick={(item) => {
-                                    item.domEvent.preventDefault();
-                                    setItemToActive([item]);
-                                    window.location.assign(`${window.location.origin}/settings`)
-                                    
-                                }}
-                                key="4"
-                                icon={<SettingOutlined />}
-                            >
-                                Settings
-                            </Menu.Item>
-                        </Menu>
-                    </Sider>
-                    <Layout>
-                        <Switch>
-                            <Route path="/steps" component={Steps} />
-                            <Route path="/tracker" component={Tracker} />
-                        </Switch>
-                    </Layout>
-                </Layout>
-            </Router>
-        </>
+                        Home
+                    </Menu.Item>
+                    <Menu.Item
+                        onClick={(item) => {
+                            item.domEvent.preventDefault();                                   
+                            history.push(`/steps`)
+                            setItemToActive([item]);
+                        }}
+                        key="2"
+                        icon={<PieChartOutlined />}
+                    >
+                        Steps
+                    </Menu.Item>
+                    <Menu.Item
+                        onClick={(item) => {
+                            item.domEvent.preventDefault();                               
+                            history.push(`/tracker`)
+                            setItemToActive([item])
+                        }}
+                        key="3"
+                        icon={<LineChartOutlined />}
+                    >
+                        Tracker
+                    </Menu.Item>
+                    <Menu.Item
+                        onClick={(item) => {
+                            item.domEvent.preventDefault();
+                            history.push(`/settings`)
+                            setItemToActive([item]);
+                        }}
+                        key="4"
+                        icon={<SettingOutlined />}
+                    >
+                        Settings
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout>
+                <Switch component={App}>
+                    <Route path="/steps" component={Steps} />
+                    <Route path="/tracker" component={Tracker} />
+                </Switch>
+            </Layout>
+        </Layout>
     );
 }
 
