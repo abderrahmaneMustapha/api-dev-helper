1ed050f7c71ac95f4e11fc6882b0c059c09b4936
abderrahmane <abderrahmaneMustapha030898@gmail.com>
2021-06-27 05:01:44 +0100

    feat : add settings page to connect to github and trello

--- web/src/main/pages/settings/stepsSettings/steps.js
+++ web/src/main/pages/settings/stepsSettings/steps.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Row, Col } from "antd";
+import { Card, Row, Col, Button } from "antd";
 const tools = [
     {
         icon: "https://ik.imagekit.io/ykyl2djdvw/WebApiHelperTool/kisspng-github-computer-icons-icon-design-github-5ab8a31e5b5395.6758034915220498223741_i0Mru_jNK.png",
@@ -35,9 +35,13 @@ const StepsSettings = () => {
                             
                             bordered={false}
                             cover={<img alt={tool.name} src={tool.icon} />}
+                          
                         >
-                        <div style={{textAlign:"center"}}>{tool.name}</div>
-                            
+                        <div style={{textAlign:"left"}}>{tool.name}</div>
+                        <div style={{display:"flex", flexDirection:"column", padding:"1em"}}>
+                            <Button type="primary">Connect</Button>    
+                        </div>
+                        
                         </Card>
                     </Col>)
                     )
